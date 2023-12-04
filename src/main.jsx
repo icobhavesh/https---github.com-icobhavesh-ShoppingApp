82c1d973e46c41931d1d5ea9467bb3530e048799
icobhavesh <bhaveshsaxena058@gmail.com>
2023-12-04 15:47:21 +0530

    "changes are made"

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import Home from './E-commerce/Home.jsx'
-// import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux/es/exports.js'
-// import Navbars from './Dummy.jsx'
+// import { Provider } from 'react-redux/es/exports.js'
+import { Provider } from 'react-redux'
 import { Store } from "../src/E-commerce/ReactRedux/CardSlice/Store.jsx"
-// import Header from './Demoproject/Header.jsx'
-// import Home_1 from './Demoproject/Home_1.jsx'
-// import { NavBar } from './Demoproject/NavBar.jsx'
-// import Hero from './Demoproject/Hero.jsx'
-// import Motion from './Demoproject/Motion.jsx'
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -23,8 +15,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>,
 )
 
-// {/* <NavBar/> */}
-// {/* <Motion/> */}
-// {/* <Home_1/> */}
-// {/* <Hero/> */}
-// {/* <Navbars/> */}
