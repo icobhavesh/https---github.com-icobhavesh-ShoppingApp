499499f6bf69b6c7ba972736bf67d871bb614904
icobhavesh <bhaveshsaxena058@gmail.com>
2023-12-04 16:48:10 +0530

    changes are made

--- vite.config.js
+++ vite.config.js
@@ -22,29 +22,29 @@
 //     ,
 //   },
 // });
-// import { defineConfig } from 'vite'
-
-// export default defineConfig({
-//   build: {
-//     rollupOptions: {
-//       external: ['react-redux']
-//     }
-//   }
-// })
-import reactRefresh from '@vitejs/plugin-react-refresh';
-import { defineConfig } from 'vite';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import nodeResolve from '@rollup/plugin-node-resolve';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [
-    reactRefresh(),
-    peerDepsExternal(),
-    nodeResolve(),
-  ],
   build: {
     rollupOptions: {
-      external: ['react-redux', 'react-router-dom', '@reduxjs/toolkit'],
-    },
-  },
-});
+      external: ['react-redux']
+    }
+  }
+})
+// import reactRefresh from '@vitejs/plugin-react-refresh';
+// import { defineConfig } from 'vite';
+// import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+// import nodeResolve from '@rollup/plugin-node-resolve';
+
+// export default defineConfig({
+//   plugins: [
+//     reactRefresh(),
+//     peerDepsExternal(),
+//     nodeResolve(),
+//   ],
+//   build: {
+//     rollupOptions: {
+//       external: ['react-redux', 'react-router-dom', '@reduxjs/toolkit'],
+//     },
+//   },
+// });
