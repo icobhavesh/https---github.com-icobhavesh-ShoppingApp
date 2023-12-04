f6da641475bf782f256e0ba3e64d91f2eff1f5af
icobhavesh <bhaveshsaxena058@gmail.com>
2023-12-04 18:02:06 +0530

    commit

--- src/E-commerce/ReactRedux/CardSlice/Store.jsx
+++ src/E-commerce/ReactRedux/CardSlice/Store.jsx
@@ -1,4 +1,4 @@
-import {configureStore} from "@reduxjs/toolkit"
+import { configureStore } from "@reduxjs/toolkit"
 import cartReducer from "../CardSlice/Cardslice"
 export const Store =configureStore({
     reducer:{
