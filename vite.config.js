fafd70de974a56c4a4d831849e0f34587faa8648
icobhavesh <bhaveshsaxena058@gmail.com>
2023-12-05 13:36:31 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
-// // import { defineConfig } from 'vite'
-// // import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // // https://vitejs.dev/config/
-// // export default defineConfig({
-// //   plugins: [react()],
-// // })
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-redux', 'react-router-dom', '@reduxjs/toolkit']
-    }
-  },
-});
+})
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     rollupOptions: {
+//       external: ['react-redux', 'react-router-dom', '@reduxjs/toolkit']
+//     }
+//   },
+// });
 
 // import { defineConfig } from 'vite'
 
